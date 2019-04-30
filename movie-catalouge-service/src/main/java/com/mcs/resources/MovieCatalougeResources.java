e4cb6a815f894411b705966963a4a1dcccb268c0
Kulwant Singh <singh-kulwant@gmx.com>
2019-05-01 00:24:25 +0530

    [APRIL|2019] #3 movie info service created

--- movie-catalouge-service/src/main/java/com/mcs/resources/MovieCatalougeResources.java
+++ movie-catalouge-service/src/main/java/com/mcs/resources/MovieCatalougeResources.java
@@ -1,5 +1,20 @@
 package com.mcs.resources;
 
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/catalog")
 public class MovieCatalougeResources {
 
+	@RequestMapping("/{userId}")
+	public List<CatalougeItem> getCatalouge(@PathVariable("userId") String userId) {
+		
+		return Collections.singletonList(new CatalougeItem("Shutter Island", "fav movie", 4));
+	}
 }
---
+++ movie-information-service/src/main/java/com/mis/resources/MovieInformationResources.java
@@ -0,0 +1,15 @@
+package com.mis.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieInformationResources {
+
+	@RequestMapping("/{movieId}")
+	public Movie getMovie(@PathVariable("movieId") String movieId) {
+		return new Movie(movieId, "Test name");
+	}
+}
