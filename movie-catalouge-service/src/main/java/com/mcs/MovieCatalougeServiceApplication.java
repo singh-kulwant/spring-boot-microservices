66861367c1d3ae8961d6bd9a067033041c279040
Kulwant Singh <singh-kulwant@gmx.com>
2019-04-30 23:54:00 +0530

    [APRIL|16.2] #2 Movie Catalouge Resources created

--- movie-catalouge-service/src/main/java/com/mcs/MovieCatalougeServiceApplication.java
+++ movie-catalouge-service/src/main/java/com/mcs/MovieCatalougeServiceApplication.java
@@ -1,4 +1,4 @@
-package com.ks.moviecatalougeservice;
+package com.mcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ movie-catalouge-service/src/main/java/com/mcs/resources/MovieCatalougeResources.java
@@ -0,0 +1,5 @@
+package com.mcs.resources;
+
+public class MovieCatalougeResources {
+
+}
