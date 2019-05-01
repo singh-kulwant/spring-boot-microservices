de14a736090bf5ac939aefceedd8cf1d7ebfb42d
Kulwant Singh <singh-kulwant@gmx.com>
2019-05-02 00:38:48 +0530

    [MAY|2019] #7 webflux web client impl

--- movie-catalouge-service/src/main/java/com/mcs/MovieCatalougeServiceApplication.java
+++ movie-catalouge-service/src/main/java/com/mcs/MovieCatalougeServiceApplication.java
@@ -2,10 +2,22 @@ package com.mcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalougeServiceApplication {
 
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
+	public WebClient.Builder getWebClient() {
+		return WebClient.builder();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalougeServiceApplication.class, args);
 	}
--- movie-catalouge-service/src/main/java/com/mcs/resources/MovieCatalougeResources.java
+++ movie-catalouge-service/src/main/java/com/mcs/resources/MovieCatalougeResources.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.mcs.models.CatalougeItem;
 import com.mcs.models.Movie;
@@ -17,10 +19,14 @@ import com.mcs.models.Rating;
 @RequestMapping("/catalog")
 public class MovieCatalougeResources {
 
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private WebClient.Builder webClient;
+
 	@RequestMapping("/{userId}")
 	public List<CatalougeItem> getCatalouge(@PathVariable("userId") String userId) {
-
-		RestTemplate restTemplate = new RestTemplate();
 
 		/* Get all movie Ids */
 		List<Rating> ratings = new ArrayList<Rating>();
@@ -28,7 +34,15 @@ public class MovieCatalougeResources {
 		ratings.add(new Rating("002", 4));
 
 		return ratings.stream().map(rating -> {
+
 			Movie movie = restTemplate.getForObject("http://localhost:9090/movies/" + rating.getMovieId(), Movie.class);
+
+			/*
+			 * Webflux webcient impl
+			 * 
+			 * Movie movie = webClient.build() .get() .uri("http://localhost:9090/movies/" +
+			 * rating.getMovieId()) .retrieve() .bodyToMono(Movie.class) .block();
+			 */
 			return new CatalougeItem(movie.getName(), "Default movie desc", rating.getRating());
 		}).collect(Collectors.toList());
 
