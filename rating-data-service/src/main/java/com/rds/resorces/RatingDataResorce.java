b5f1c7348688df66ef836262815cb09d74a34f01
Kulwant Singh <singh-kulwant@gmx.com>
2019-05-02 23:41:01 +0530

    [MAY|2019] #9 format

--- rating-data-service/src/main/java/com/rds/resorces/RatingDataResorce.java
+++ rating-data-service/src/main/java/com/rds/resorces/RatingDataResorce.java
@@ -14,16 +14,16 @@ import com.rds.models.Rating;
 public class RatingDataResorce {
 
 	@RequestMapping("/{movieId}")
-    public Rating getRating(@PathVariable("movieId") String movieId) {
-        return new Rating(movieId, 4);
-    }
-	
+	public Rating getRating(@PathVariable("movieId") String movieId) {
+		return new Rating(movieId, 4);
+	}
+
 	@RequestMapping("users/{userId}")
 	public List<Rating> getUserRatings(@PathVariable("userId") String userId) {
 		List<Rating> ratings = new ArrayList<Rating>();
 		ratings.add(new Rating("001", 3));
 		ratings.add(new Rating("002", 4));
-		
+
 		return ratings;
 	}
 }
