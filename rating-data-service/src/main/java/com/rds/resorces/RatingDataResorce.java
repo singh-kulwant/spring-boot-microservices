b200a2314e8666eaa053906e6cfaf633459ccd35
Kulwant Singh <singh-kulwant@gmx.com>
2019-05-02 23:37:16 +0530

    [MAY|2019] #8 updated rds

--- rating-data-service/src/main/java/com/rds/resorces/RatingDataResorce.java
+++ rating-data-service/src/main/java/com/rds/resorces/RatingDataResorce.java
@@ -1,5 +1,8 @@
 package com.rds.resorces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +16,14 @@ public class RatingDataResorce {
 	@RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
-    }	
+    }
+	
+	@RequestMapping("users/{userId}")
+	public List<Rating> getUserRatings(@PathVariable("userId") String userId) {
+		List<Rating> ratings = new ArrayList<Rating>();
+		ratings.add(new Rating("001", 3));
+		ratings.add(new Rating("002", 4));
+		
+		return ratings;
+	}
 }
